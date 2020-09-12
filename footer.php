5cb37a4ddb2fa421424082c08f7673fbaa9df23a
Lukas Oertel <git@luoe.dev>
2020-09-12 12:18:14 +0200

    Add a reference to source code

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
 <div id="footer">
-  <p>handmade with <i class="far fa-heart"></i> by <a href="https://github.com/elogy/">elogy</a> for <a href="https://github.com/fsi-tue/">fsi</a></p>
+  <p><a href="https://github.com/fsi-tue/kki">handmade</a> with <i class="far fa-heart"></i> by <a href="https://github.com/elogy/">elogy</a> for <a href="https://github.com/fsi-tue/">fsi</a></p>
 </div>
 </body>
